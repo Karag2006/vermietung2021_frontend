d39df4423ca71a10d4e1df6bd024777c22a1285f
Karag <karag2006@gmx.net>
2021-09-09 15:05:57 +0200

    nav Menu visuals

--- src/components/layout/NavMenu.vue
+++ src/components/layout/NavMenu.vue
@@ -1,19 +1,19 @@
 <template>
     <v-list dark dense>
         <div v-for="entry in $store.state.navigation.navMenu" :key="entry.id">
-            <v-list-item v-if="!entry.children">
+            <v-list-item v-if="!entry.children && entry.link" :to="entry.link">
                 <v-list-item-icon>
                     <v-icon>{{ entry.icon }}</v-icon>
                 </v-list-item-icon>
 
                 <v-list-item-title>{{ entry.name }}</v-list-item-title>
             </v-list-item>
-            <v-list-group v-else :prepend-icon="entry.icon">
+            <v-list-group v-else :prepend-icon="entry.icon" active-class="v-list-item--active">
                 <template v-slot:activator>
                     <v-list-item-title>{{ entry.name }}</v-list-item-title>
                 </template>
                 <div v-for="child in entry.children" :key="child.id">
-                    <v-list-item class="ml-5">
+                    <v-list-item class="pl-12" v-if="child.link" :to="child.link">
                         <v-list-item-icon>
                             <v-icon>{{ child.icon }}</v-icon>
                         </v-list-item-icon>
@@ -32,4 +32,12 @@ export default {
 };
 </script>
 
-<style></style>
+<style scoped>
+    >>>.v-list-group .v-list-group__header .v-list-item__icon.v-list-group__header__append-icon {
+        min-width: 0;
+        margin-left: 0;
+    }
+    .v-application .primary--text{
+        color: inherit !important;
+    }
+</style>
